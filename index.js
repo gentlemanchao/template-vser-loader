67f42b6235ebeb625c0a576b5286bc677d341ac1
caojianyu <caojianyu@zyctd.com>
2020-07-30 14:44:59 +0800

    first commit

---
+++ index.js
@@ -0,0 +1,22 @@
+const Parse = require('./src/parse');
+
+/**
+ * 移除注释
+ * @param {*} content 
+ */
+var removeRemark = function (content) {
+    const reg = /<!--[\s\S]*?-->/g
+    return content.replace(reg, '');
+}
+
+
+module.exports = function (source) {
+    this.cacheable && this.cacheable();
+    source = removeRemark(source);
+    const ast = new Parse(source);
+    let funcStr = ast.render();
+    console.log('---funcStr:')
+    console.log(funcStr);
+    funcStr = funcStr.replace(/[\r\n]/g, '');
+    return `module.exports = function(){${funcStr}}`;
+};
